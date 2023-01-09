aed9b65e2c3cd7f4e5a08e6df78f3cd670a54059
rashidsarkar <rashidrock558@gmail.com>
2023-01-09 19:16:01 +0600

    ok
    ok

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$("#slider").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+});
+$(".testimonial_slider").slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  arrows: false,
+});
+$(".counter").counterUp({
+  delay: 10,
+  time: 1000,
+});
